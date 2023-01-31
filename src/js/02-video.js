304827180791173edddd7e7ab8524f88d550654b
Volodymyr <110925314+woland31252@users.noreply.github.com>
2023-01-31 21:42:29 +0200

    update task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,36 +1,28 @@
 
 import Player from '@vimeo/player';
-
-const player = new Player('handstick', {
-  id: 19231868,
-  width: 640,
+import throttle from 'lodash.throttle';
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+player.on('timeupdate', throttle(onPlay, 1000));
+function onPlay(e) {
+  console.log(e.seconds);
+  const time = e.seconds;
+  localStorage.setItem('videoplayer-current-time', time);
+}
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
 });
-
-player.on('play', function () {
-  console.log('played the video!');
-});
-
-// const iframe = document.querySelector('iframe');
-// const player = new Player(iframe);
-// const CURRENT_TIME = 'videoplayer-current-time';
-
-// const onPlay = function (data) {
-//   localStorage.setItem(CURRENT_TIME, data.seconds);
-// };
-
-// player.on('timeupdate', throttle(onPlay, 1000));
-
-// const savedTime = localStorage.getItem(CURRENT_TIME);
-// if (savedTime) {
-//   player.setCurrentTime(savedTime);
-// }
-const vimPlayer = document.getElementById('vimeo-player');
-const curentTime = 'videoplayer-current-time';
-const onPlay = function (data) {
-  // data is an object containing properties specific to that event
-};
-player.on('eventName', function (data) {
-  // data is an object containing properties specific to that event
-});
-
-player.on('play', onPlay);
+const currentTame = localStorage.getItem('videoplayer-current-time');
+const startTame = JSON.parse(currentTame);
+console.log(startTame);
+player
+  .setCurrentTime(currentTame)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
