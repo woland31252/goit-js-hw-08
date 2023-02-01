2cdc6a00254912cfa1c10fa0e78edaffadf58060
Volodymyr <110925314+woland31252@users.noreply.github.com>
2023-02-01 23:53:07 +0200

    TASK-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,2 +1,42 @@
 import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+console.log(formEl);
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+updateForm();
+
+formEl.addEventListener('input', throttle(onFormInput, 500));
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  console.log(formData);
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const {
+    elements: { email, message },
+  } = e.target;
+
+  if (email.value === '' || message.value === '') {
+    return window.alert('Please fill in all the fields!');
+  }
+  console.log({ Email: email.value, Message: message.value });
+  formEl.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function updateForm() {
+  if (localStorage.getItem(STORAGE_KEY) === null) {
+    return;
+  }
+  const savedForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  Object.entries(savedForm).forEach(([name, value]) => {
+    formData[name] = value;
+    formEl.elements[name].value = value;
+  });
+}
